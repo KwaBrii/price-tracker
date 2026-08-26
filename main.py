32b7035ef1266e948a5336ee99b2d44e7fac2609
Brii <nathaliab.locatelli@gmail.com>
2026-08-26 16:46:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-import sys
-print(sys.executable)
-
 import requests
 from bs4 import BeautifulSoup
 
+## URL Teste
 url = "https://www.amazon.com.br/Marcador-Uni-Ball-58-9200-Multicor-Pacote/dp/B07S7K1JHZ"
-
-headers = {
-  "User-Agent": "Mozilla/5.0"
-}
 
 response = requests.get(url)
 print(response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
-
 price_element = soup.find("span", class_="a-offscreen")
+
 if price_element is not None:
-    print(price_element.text)
+    price_text = price_element.text.strip()
+
+    price_text = price_text.replace("R$", "")
+    price_text = price_text.replace(".", "")
+    price_text = price_text.replace(",", ".")
+
+    price = float(price_text)
+
+    target_price = 160.00
+
+    print(f"Current price: R$ {price:.2f}")
+    print(f"Target price: R$ {target_price:.2f}")
+
+    if price <= target_price:
+        print("Target price reached!")
+    else:
+        print("The price is still above the target.")
+
 else:
     print("Price not found.")
