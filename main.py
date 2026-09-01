fcb19c442c6735afee9146a45383d7028878525f
Brii <nathaliab.locatelli@gmail.com>
2026-09-01 14:15:51 -0300

    Update main.py
    code modularization, added clean_price function and fixed some errors

--- main.py
+++ main.py
@@ -4,31 +4,29 @@ from bs4 import BeautifulSoup
 def get_price(url):
     response = requests.ge(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    price_elemente = sou.find("span", class="a-offscreen")
+    price_elemente = soup.find("span", class_="a-offscreen")
 
     if price_element is None:
         return None
     return price_element.text.strip()
 
-## URL Teste
-url = "https://www.amazon.com.br/Marcador-Uni-Ball-58-9200-Multicor-Pacote/dp/B07S7K1JHZ"
-
-response = requests.get(url)
-print(response.status_code)
-
-soup = BeautifulSoup(response.text, "html.parser")
-price_element = soup.find("span", class_="a-offscreen")
-
-if price_element is not None:
-    price_text = price_element.text.strip()
-
+def clean_price(price_text):
     price_text = price_text.replace("R$", "")
     price_text = price_text.replace(".", "")
     price_text = price_text.replace(",", ".")
 
-    price = float(price_text)
+    return float(price_text)
 
-    target_price = 160.00
+
+
+## URL Teste
+url = "https://www.amazon.com.br/Marcador-Uni-Ball-58-9200-Multicor-Pacote/dp/B07S7K1JHZ"
+target_price = 160.00
+
+price_text = get_price(url)
+
+if price_text is not None:
+    price = clean_price(price_text)
 
     print(f"Current price: R$ {price:.2f}")
     print(f"Target price: R$ {target_price:.2f}")
